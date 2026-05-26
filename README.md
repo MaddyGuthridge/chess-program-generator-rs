0fa0ab7af3d40887a60e100fa73c043c727d4d05
Maddy Guthridge <hello@maddyguthridge.com>
2026-05-27 00:36:57 +1000

    Set up project

---
+++ README.md
@@ -0,0 +1,7 @@
+# chess-program-generator-rs
+
+A 🔥blazingly-fast🔥 program to generate a chess program. It is so fast because
+it is written in Rust. Since chess is such a complex game, it is impossible to
+write a program for it by hand.
+
+This tool provides a CLI to generate a simple terminal chess program.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use clap::Parser;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Number of times to greet
+    #[arg(short, long)]
+    depth: u32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("Generating to a depth of {}", args.depth);
 }
